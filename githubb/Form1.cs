c70df07200a703766c7af7e4ecb388b4d66e175b
micinj.02 <micinj.02@B121-11>
2021-05-07 08:45:38 +0200

    zmena1

--- githubb/Form1.cs
+++ githubb/Form1.cs
@@ -19,7 +19,7 @@ namespace githubb
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ahoj");
+            MessageBox.Show("ahaoj");
         }
     }
 }
