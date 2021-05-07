3028f19f632cc505d42cb48eb717ea517cab54eb
micinj.02 <micinj.02@B121-11>
2021-05-07 09:19:59 +0200

    novazmena

--- githubb/Form1.cs
+++ githubb/Form1.cs
@@ -21,5 +21,10 @@ namespace githubb
         {
             MessageBox.Show("Dobrý den");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Budiž zdráv cestovateli");
+        }
     }
 }
