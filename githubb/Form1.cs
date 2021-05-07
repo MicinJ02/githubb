97b2cb17ed70b6335857f6cf8c89169bbe1bb651
micinj.02 <micinj.02@B121-11>
2021-05-07 08:43:25 +0200

    zmena2

--- githubb/Form1.cs
+++ githubb/Form1.cs
@@ -16,5 +16,10 @@ namespace githubb
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ahoj");
+        }
     }
 }
